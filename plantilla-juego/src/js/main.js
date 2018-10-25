2966ce00344dcadc8437155a353624616ae3cc91
aibort <37537162+aibort@users.noreply.github.com>
2018-10-25 19:37:28 +0200

    Bullet y raton

--- plantilla-juego/src/js/main.js
+++ plantilla-juego/src/js/main.js
@@ -15,7 +15,7 @@ var BootScene = {
 };
 
 
-var PreloaderScene = {
+/*var PreloaderScene = {
   preload: function () {
     this.loadingBar = this.game.add.sprite(0, 240, 'preloader_bar');
     this.loadingBar.anchor.setTo(0, 0.5);
@@ -28,8 +28,112 @@ var PreloaderScene = {
   create: function () {
     this.game.state.start('play');
   }
-};
+}; */
 
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+
+function preload() {
+    game.load.image('arrow', 'assets/sprites/arrow.png');
+    game.load.image('player', 'images/singletoad.png');
+}
+
+var sprite;
+
+function create() {
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    game.stage.backgroundColor = '#0072bc';
+
+    sprite = game.add.sprite(400, 300, 'arrow');
+    sprite = game.add.sprite(400, 300, 'player');
+    sprite.anchor.setTo(0.5, 0.5);
+
+    //  Enable Arcade Physics for the sprite
+    game.physics.enable(sprite, Phaser.Physics.ARCADE);
+
+    //  Tell it we don't want physics to manage the rotation
+    sprite.body.allowRotation = false;
+
+}
+
+function update() {
+
+    sprite.rotation = game.physics.arcade.moveToPointer(sprite, 60, game.input.activePointer, 500);
+
+}
+
+function render() {
+
+    game.debug.spriteInfo(sprite, 32, 32);
+
+}
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+
+function preload() {
+
+    game.load.image('caparazon', 'images/caparazonverde.png');
+    game.load.image('player', 'images/singletoad.png');
+
+}
+
+var sprite;
+var weapon;
+var cursors;
+var fireButton;
+
+function create() {
+
+    //  Creates 1 single bullet, using the 'bullet' graphic
+    weapon = game.add.weapon(1, 'caparazon');
+
+    //  The bullet will be automatically killed when it leaves the world bounds
+    weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+
+    //  Because our bullet is drawn facing up, we need to offset its rotation:
+    weapon.bulletAngleOffset = 90;
+
+    //  The speed at which the bullet is fired
+    weapon.bulletSpeed = 400;
+
+    sprite = this.add.sprite(320, 500, 'player');
+
+    game.physics.arcade.enable(sprite);
+
+    //  Tell the Weapon to track the 'player' Sprite, offset by 14px horizontally, 0 vertically
+    weapon.trackSprite(sprite, 14, 0);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    fireButton = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+
+}
+
+function update() {
+
+    sprite.body.velocity.x = 0;
+
+    if (cursors.left.isDown)
+    {
+        sprite.body.velocity.x = -200;
+    }
+    else if (cursors.right.isDown)
+    {
+        sprite.body.velocity.x = 200;
+    }
+
+    if (fireButton.isDown)
+    {
+        weapon.fire();
+    }
+
+}
+
+function render() {
+
+    weapon.debug();
+
+}
 
 window.onload = function () {
   var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');

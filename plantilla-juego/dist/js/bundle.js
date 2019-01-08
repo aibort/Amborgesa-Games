0a8a0ebc13b5bdd414f132397608c20aa87b0c86
alvarded <alvarded@ucm.es>
2019-01-08 23:36:58 +0100

    Dist

--- plantilla-juego/dist/js/bundle.js
+++ plantilla-juego/dist/js/bundle.js
@@ -1,8 +1,26 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 'use strict';
 
+//constructor
+var gameObject = function(game, sprite, posX, posY, anchorX, anchorY, scaleX, sacaleY)
+{ 
+  this.sprite = game.add.sprite(posX, posY, sprite);
+  this.sprite.anchor.set(anchorX, anchorY);
+  this.sprite.scale.setTo(scaleX, sacaleY);
+  
+  game.physics.enable(this.sprite,Phaser.Physics.ARCADE);
+}
+
+module.exports=
+{
+    gameObject    
+}
+},{}],2:[function(require,module,exports){
+'use strict';
+
 var PlayScene = require('./play_scene.js');
-var CAR= require('./player.js');
+var Menu = require('./menu.js');
+//var CAR= require('./player.js');
 
 var BootScene = {
   preload: function () {
@@ -15,7 +33,6 @@ var BootScene = {
   }
 };
 
-
 var PreloaderScene = {
   preload: function () {
     this.loadingBar = this.game.add.sprite(0, 240, 'preloader_bar');
@@ -26,12 +43,13 @@ var PreloaderScene = {
     this.game.load.image('logo', 'images/phaser.png');
     this.game.load.image('toad', 'images/mapa.png');
     this.game.load.image('redcar', 'images/redcar.png');
-    this.game.load.tilemap('mapa', 'images/mapa/mapa.json', null, Phaser.Tilemap.TILED_JSON);
-    this.game.load.image('earth', 'images/Earth.png');
+    this.game.load.image('bluecar', 'images/bluecar.png');
+    this.game.load.spritesheet('play', 'images/play.png', 194, 100, 2);
+    this.game.load.image('fondomenu', 'images/fondomenu.jpg');
   },
 
   create: function () {
-    this.game.state.start('play');
+    this.game.state.start('menu');
   }
 };
 
@@ -41,139 +59,145 @@ window.onload = function () {
   game.state.add('boot', BootScene);
   game.state.add('preloader', PreloaderScene);
   game.state.add('play', PlayScene);
+  game.state.add('menu', Menu);
 
   game.state.start('boot');
 };
 
-},{"./play_scene.js":2,"./player.js":3}],2:[function(require,module,exports){
+},{"./menu.js":3,"./play_scene.js":4}],3:[function(require,module,exports){
 'use strict';
 
-var CAR= require('./player.js');
+var Menu = {
+
+    create:function(){   
+        this.background = this.game.add.image(0, 0, 'fondomenu');
+
+        this.playButton = this.game.add.button(300, 200, 'play', this.play, this, 1, 0, 1);
+    },
+    play: function(){
+        this.game.state.start('play');
+    },
+
+};
+
+module.exports= Menu;
+},{}],4:[function(require,module,exports){
+  'use strict';
+
+var jugadores= require('./player.js');
+var objeto= require('./gameObject.js');
 
 var PlayScene = {
 
   create: function () {
    
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
-    this.game.world.setBounds(0,0,10000,10000);
+    this.game.world.setBounds(0,0,2220,2220);
 
-   /* this.map = this.game.add.tilemap('mapa');
-    this.map.addTilesetImage('Earth', 'earth');
-    
-    this.suelo = this.map.createLayer('suelo');
-    this.muros = this.map.createLayer('muros');
-    this.suelo.resizeWorld();
-    this.muros.resizeWorld();*/
-
+    //circuito
     var logo = this.game.add.sprite(
       this.game.world.centerX, this.game.world.centerY, 'toad');
     logo.anchor.setTo(0.5, 0.5);
     logo.scale.setTo(4.2,4.2);
-
-    this.jugador = new CAR.player(this.game, 'redcar', this.game.world.centerX, this.game.world.centerY, 0.5, 0.5, 0.1, 0.1);
+    
+    //jugadores
+    this.jugador = new jugadores.player(this.game, 'redcar', this.game.world.centerX-300, this.game.world.centerY+390, 0.4, 0.5, 0.1, 0.1);
+    this.jugador2 = new jugadores.player(this.game, 'bluecar', this.game.world.centerX-300, this.game.world.centerY+430, 0.4, 0.5, 0.1, 0.1);
+    //cursores jugador 1
     this.cursors = this.game.input.keyboard.createCursorKeys();
+    //cursores jugador 2
+    this.wasd = {
+      up: this.game.input.keyboard.addKey(Phaser.Keyboard.W),
+      down: this.game.input.keyboard.addKey(Phaser.Keyboard.S),
+      left: this.game.input.keyboard.addKey(Phaser.Keyboard.A),
+      right: this.game.input.keyboard.addKey(Phaser.Keyboard.D),
+    };
   },
 
   update: function() {
-    //UPDATE DE MOVIMIENTO    
+    //update de los jugadores  
     this.jugador.update(this.cursors, this.game);
+    this.jugador2.update(this.wasd, this.game);
+   
+    this.game.physics.arcade.collide(this.jugador.sprite, this.jugador2.sprite);
       
-    this.game.camera.follow(this.jugador.sprite, Phaser.Camera.FOLLOW_LOCKON, 0.7, 0.7); 
+    this.game.camera.follow(this.jugador.sprite); 
   },
 
   render: function() {
-    //game.debug.cameraInfo(game.camera, 32, 32);
-  }
+  },
+
+  fin: function(){
+    this.game.state.start('menu');
+  },
 };
 
 
 module.exports = PlayScene;
 
-},{"./player.js":3}],3:[function(require,module,exports){
-//CONSTRUCTOR DE ELEMENTOS DEL MAPA
-var gameObject = function(game, sprite, posX, posY, anchorX, anchorY, scaleX, sacaleY)
-{
-  
-  this.sprite = game.add.sprite(posX, posY, sprite);
-  this.sprite.anchor.set(anchorX, anchorY);
-  this.sprite.scale.setTo(scaleX, sacaleY);
-  game.physics.enable(this.sprite,Phaser.Physics.ARCADE);
-  
-  this.sprite.body.immovable = true;
-  this.sprite.body.colliderWorldBounds = true;
-  this.sprite.body.bounce.setTo(1, 1);
-  this.sprite.allowRotation = true;
-  this.sprite.angle += -90;
-}
+},{"./gameObject.js":1,"./player.js":5}],5:[function(require,module,exports){
+'use strict';
 
-//CONSTRUCTOR DE VEHICULOS
-var vehicle = function(game, sprite, posX, posY, anchorX, anchorY, scaleX, sacaleY)
-{
-  this.game = game
-  this.velocity = 0;
-  this.acceleration = 5;
-  this.MaxVelocity = 900;
-  this.MinVelocity =-200;  
-  this.alive = true;
+var objeto= require('./gameObject.js');
 
- 
-  gameObject.call(this, game, sprite, posX, posY, anchorX, anchorY, scaleX, sacaleY);
-};
-vehicle.prototype = Object.create(gameObject.prototype);
-vehicle.prototype.constructor = vehicle;
-
-//CONSTRUCTOR DE PLAYER
+//constructor
 var player=function(game, sprite, posX, posY, anchorX, anchorY, scaleX, sacaleY)
 {
   this.game=game;
-  vehicle.call(this, game, sprite, posX, posY, anchorX, anchorY, scaleX, sacaleY);
+  this.maxVel = 600;
+  this.minVel =-300; 
+  this.velocity = 0;
+  this.acceleration = 6;
+  
+  objeto.gameObject.call(this, game, sprite, posX, posY, anchorX, anchorY, scaleX, sacaleY);
 };
-player.prototype = Object.create(vehicle.prototype);
+player.prototype = Object.create(objeto.gameObject.prototype);
 player.prototype.constructor = player;
 
-//PLAYER UPDATE
+//update de player
 player.prototype.update = function(cursors,game)
 {
-
+  //rotación del coche, siempre que esté en movimiento
   if(this.velocity!=0)
   {
-  if(cursors.left.isDown){ this.sprite.angle -= 2; }
+  if(cursors.left.isDown){ this.sprite.angle = this.sprite.angle - 4; }
 
-  else if(cursors.right.isDown){ this.sprite.angle += 2; }
+  else if(cursors.right.isDown){ this.sprite.angle = this.sprite.angle + 4; }
   }
 
+  //aceleración hasta alcanzar la velocidad maxima
    if(cursors.up.isDown)
   { 
-    this.velocity += this.acceleration; 
+    this.velocity = this.velocity + this.acceleration; 
 
-    if(this.velocity > this.MaxVelocity)
-    this.velocity = this.MaxVelocity;
+    if(this.velocity > this.maxVel)
+    this.velocity = this.maxVel;
   }
+  //deceleración hasta alcanzar la velocidad minima
   else if(cursors.down.isDown)
   { 
-    this.velocity -= this.acceleration*3; 
+    this.velocity = this.velocity - this.acceleration; 
 
-    if(this.velocity < this.MinVelocity)
-    this.velocity = this.MinVelocity;
+    if(this.velocity < this.minVel)
+    this.velocity = this.minVel;
   }
+
+  //frenado del coche cuando se deja de pulsar el acelerador o el decelerador
   else if(this.velocity>0)
   {
-    this.velocity-=this.acceleration;
+    this.velocity= this.velocity - this.acceleration;
   }
 
   else if(this.velocity<0)
   {
-    this.velocity+=this.acceleration;
+    this.velocity = this.velocity + this.acceleration;
   } 
 
-  game.physics.arcade.velocityFromRotation(this.sprite.rotation, this.velocity, this.sprite.body.velocity); 
-  //game.world.wrap(this.sprite, 16);
+  game.physics.arcade.velocityFromRotation(this.sprite.rotation, this.velocity, this.sprite.body.velocity);
 };
 
 module.exports=
-{
-    gameObject,
-    vehicle,
+{   
     player
 }
-},{}]},{},[1]);
+},{"./gameObject.js":1}]},{},[2]);
